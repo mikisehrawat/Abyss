6890f119e339406d8de6e26e1cda834fcb27c1e8
mikisehrawat <sahiljaatbirohar@gmail.com>
2026-01-11 18:39:59 +0530

    Setup

---
+++ src/main/java/org/example/abyss/AbyssApplication.java
@@ -0,0 +1,13 @@
+package org.example.abyss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbyssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbyssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/abyss/AbyssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.abyss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbyssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
